c82948aa98748bd4a8d059c697aa181495f95409
zne00 <202011145@gordoncollege.edu.ph>
2022-11-22 13:30:02 +0800

    Started Main Menu

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-import pygame, sys
-from button import Button
+from game import Game
 
-pygame.init()
+g = Game()
 
-SCREEN = pygame.display.set_mode((1280, 900))
-pygame.display.set_caption("KALI THE SAUSAGE")
-
-BG = pygame.image.load("assets/BG/Clouudy.png")
-
-def main_menu(): #Main Menu Screen
-    pygame.display.set_caption("MENU")
-
-    while True:
-        SCREEN.blit(BG, (0, 0))
-
-        MENU_MOUSE_POS = pygame.mouse.get_pos()
-
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
-
-        PLAY_BUTTON = Button(image=pygame.image.load())
+while g.running:
+    g.curr_menu.display_menu()
+    g.game_loop()
