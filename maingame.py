5bb03db305a34ec839af8d1bce656177103dc589
zne00 <202011145@gordoncollege.edu.ph>
2022-12-04 19:35:45 +0800

    Added death, health, collision, and throwing pebbles

--- maingame.py
+++ maingame.py
@@ -4,8 +4,8 @@ import os
 pygame.init()
 
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 900
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('KALI THE SAUSAGE')
@@ -22,6 +22,11 @@ GRAVITY = 0.75
 #define player action variables
 moving_left = False
 moving_right = False
+shoot = False
+
+#load images
+#rock
+pebble_img = pygame.image.load('img/Icons/pebble.png').convert_alpha()
 
 #define colours
 BG = (144, 201, 120)
@@ -33,30 +38,36 @@ def draw_bg():
 
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self, char_type, x, y, scale, speed):
+    def __init__(self, char_type, x, y, scale, speed, ammo):
         pygame.sprite.Sprite.__init__(self)
         self.alive = True
         self.char_type = char_type
         self.speed = speed
+        self.ammo = ammo
+        self.start_ammo = ammo
+        self.shoot_cooldown = 0
+        self.health = 100
+        self.max_health = self.health
         self.direction = 1
         self.vel_y = 0
         self.jump = False
         self.in_air = True
         self.flip = False
+        self.velocity = 50
         self.animation_list = []
         self.frame_index = 0
         self.action = 0
         self.update_time = pygame.time.get_ticks()
 
         #load all images for the player
-        animation_types = ['Idle', 'Run', 'Jump']
+        animation_types = ['Idle', 'Run', 'Jump', 'Death']
         for animation in animation_types:
             #reset temporary list of images
             temp_list = []
             #count number of files in the folder
             num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
             for i in range(num_of_frames):
-                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png')
+                img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
                 img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
                 temp_list.append(img)
             self.animation_list.append(temp_list)
@@ -64,6 +75,13 @@ class Character(pygame.sprite.Sprite):
         self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+    def update(self):
+        self.update_animation()
+        self.check_alive()
+        #update cooldown
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
 
     def move(self, moving_left, moving_right):
         # reset movement variables
@@ -101,6 +119,13 @@ class Character(pygame.sprite.Sprite):
         self.rect.x += dx
         self.rect.y += dy
 
+    def shoot(self):
+        if self.shoot_cooldown == 0 and self.ammo > 0:
+            self.shoot_cooldown = 20
+            pebble = Pebble(self.rect.centerx + (0.5 * self.rect.size[0] * self.direction), self.rect.centery, self.direction)
+            pebble_group.add(pebble)
+            #reduce ammo
+            self.ammo -= 1
 
     def update_animation(self):
         #update animation
@@ -113,7 +138,10 @@ class Character(pygame.sprite.Sprite):
             self.frame_index += 1
         #if animation has run out the reset back to the start
         if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
+            if self.action == 3:
+                self.frame_index = len(self.animation_list[self.action]) - 1
+            else:
+                self.frame_index = 0
 
 
     def update_action(self, new_action):
@@ -124,12 +152,48 @@ class Character(pygame.sprite.Sprite):
             self.frame_index = 0
             self.update_time = pygame.time.get_ticks()
 
+    def check_alive(self):
+        if self.health <= 0:
+            self.health = 0
+            self.speed = 0
+            self.alive = False
+            self.update_action(3)
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
-player = Character('Player', 200, 200, .5, 5)
-enemy = Character('Enemy', 400, 200, .5, 5)
+class Pebble(pygame.sprite.Sprite):
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 10
+        self.image = pebble_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+
+    def update(self):
+        #move pebble
+        self.rect.x += (self.direction * self.speed)
+        #check if bullet has gone off screen
+        if self.rect.right < 0 or self.rect.left > SCREEN_WIDTH:
+            self.kill()
+
+        #check collision with characters
+        if pygame.sprite.spritecollide(player, pebble_group, False):
+            if player.alive:
+                player.health -= 5
+                self.kill()
+        if pygame.sprite.spritecollide(enemy, pebble_group, False):
+            if enemy.alive:
+                enemy.health -= 25
+                print(enemy.health)
+                self.kill()
+
+#create sprite groups
+pebble_group = pygame.sprite.Group()
+
+player = Character('Player', 200, 200, .5, 5, 5)
+enemy = Character('Enemy', 400, 200, .5, 5, 20)
 
 run = True
 while run:
@@ -138,18 +202,26 @@ while run:
 
     draw_bg()
 
-    player.update_animation()
+    player.update()
     player.draw()
+
+    enemy.update()
     enemy.draw()
+
+    #update and draw groups
+    pebble_group.update()
+    pebble_group.draw(screen)
 
     #update player actions
     if player.alive:
+        if shoot:
+            player.shoot()
         if player.in_air:
-            player.update_action(2)  # 2: jump
+            player.update_action(2)     # 2: jump
         elif moving_left or moving_right:
-            player.update_action(1)#1: run
+            player.update_action(1)     # 1: run
         else:
-            player.update_action(0)  # 0: idle
+            player.update_action(0)     # 0: idle
         player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
@@ -158,21 +230,33 @@ while run:
             run = False
         #keyboard presses
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_LEFT:
                 moving_left = True
-            if event.key == pygame.K_d:
+            elif event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_RIGHT:
                 moving_right = True
-            if event.key == pygame.K_w and player.alive:
+            elif event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_w:
+                shoot = True
+            if event.key == pygame.K_SPACE and player.alive:
                 player.jump = True
             if event.key == pygame.K_ESCAPE:
                 run = False
 
         #keyboard button released
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_LEFT:
                 moving_left = False
-            if event.key == pygame.K_d:
+            elif event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_RIGHT:
                 moving_right = False
+            elif event.key == pygame.K_d:
+                moving_right = False
+            if event.key == pygame.K_w:
+                shoot = False
 
     pygame.display.update()
 
